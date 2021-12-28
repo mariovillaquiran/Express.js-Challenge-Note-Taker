8e6a123968102c9bb0fe450689a0e5098eb5707f
Mario Villaquiran <mariovillaquiran@Marios-MacBook-Air.local>
2021-12-28 17:29:29 -0500

    worked on htmlroute.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const apiroute = require ("./routes/apiroute")
-const htmlroute = require ("./routes/htmlroutes")
-const app = express()
-const PORT = process.env.PORT || 3001
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(express.static("public"))
-app.use("/api", apiroute)
-app.use("/", htmlroute)
-app.listen(PORT,() => console.log(PORT))
+const apiroute = require ("./routes/apiroute");
+const htmlroute = require ("./routes/htmlroutes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use("/api", apiroute);
+app.use("/", htmlroute);
+app.listen(PORT,() => console.log(PORT));
